b91bd8b0c761bf1812dfc86e7b7ccc8bb7c5d00b
amenesca <alanmenescal@hotmail.com>
2024-04-01 15:25:09 -0300

    Mudanças feitas no writePipes

--- srcs/Client/Client.cpp
+++ srcs/Client/Client.cpp
@@ -5,252 +5,287 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: amenesca <amenesca@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/21 14:33:07 by amenesca          #+#    #+#             */
-/*   Updated: 2024/03/27 19:34:46 by amenesca         ###   ########.fr       */
+/*   Created: 2024/01/05 00:37:17 by femarque          #+#    #+#             */
+/*   Updated: 2024/04/01 15:24:48 by amenesca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./Client.hpp"
+#include "CgiHandler.hpp"
 
-Client::Client() :
-	_clientSocket(-1),
-	_client_addr(),
-	_client_addr_len(sizeof(this->_client_addr)),
-	_requestBuffer(""),
-	_bytesRead(0),
-	_requestRead(false),
-	_firstTimeRequest(true),
-	_serverConfigs(),
-	_requestParser(),
-	_totalBodyBytes(0)
+CgiHandler::CgiHandler() :
+	_pid(0),
+	_env(),
+	_request_pipe(),
+	_request(),
+	_log()
 {
-	
+	_env = std::vector<char*>();
+	_pid = 0;
+	_request_pipe[0] = 0;
+	_request_pipe[1] = 0;
 }
 
-Client::~Client()
+CgiHandler::CgiHandler(RequestParser request) :
+	_pid(0),
+	_env(),
+	_request_pipe(),
+	_request(request),
+	_log()
 {
-//	this->_clientSocket = -1;
-	this->_client_addr_len = 0;
-	memset(&this->_client_addr,0,sizeof(this->_client_addr));
-	_requestRead = false;
-	_firstTimeRequest = false;
-	this->_requestBuffer.clear();
-	this->_bytesRead = 0;
-	this->_totalBodyBytes = 0;
+
 }
 
-Client::Client(const Client& copy)
+CgiHandler::CgiHandler(const CgiHandler& copy)
 {
 	*this = copy;
-	return ;
+	return;
 }
 
-Client& Client::operator=(const Client& src)
+CgiHandler& CgiHandler::operator=(const CgiHandler& src)
 {
 	if (this != &src)
 	{
-		this->_clientSocket = src.getClientSocket();
-		this->_client_addr_len = src.getClientAddrLen();
-		this->_client_addr = src.getClientAddr();
-		this->_requestBuffer = src.getRequestBuffer();
-		this->_bytesRead = src.getBytesRead();
-		this->_serverConfigs = src.getServerConfigs();
-		this->_requestParser = src.getRequest();
-		this->_requestRead = src.getRequestRead();
-		this->_firstTimeRequest = src.getFirstTimeRequest();
-		this->_totalBodyBytes = src.getTotalBodyBytes();
+		this->_env = src.getEnv();
+		this->_pid = src.getPid();
+		this->_request_pipe[0] = src.getRequestPipe1();
+		this->_request_pipe[1] = src.getRequestPipe2();
+		this->_request = src.getRequest();
+		this->_log = src.getLog();
 	}
 	return *this;
 }
 
-int	Client::getClientSocket(void) const
+pid_t	CgiHandler::getPid() const
 {
-	return this->_clientSocket;
+	return this->_pid;
 }
 
-socklen_t	Client::getClientAddrLen(void) const
+std::vector<char*>	CgiHandler::getEnv() const
 {
-	return this->_client_addr_len;
+	return this->_env;
 }
 
-struct sockaddr_in	Client::getClientAddr(void) const
+int		CgiHandler::getRequestPipe1() const
 {
-	return this->_client_addr;
+	return this->_request_pipe[0];
 }
 
-std::string	Client::getRequestBuffer(void) const
+int		CgiHandler::getRequestPipe2() const
 {
-	return this->_requestBuffer;
+	return this->_request_pipe[1];
 }
 
-ssize_t	Client::getBytesRead(void) const
+RequestParser 		CgiHandler::getRequest() const
 {
-	return this->_bytesRead;
+	return this->_request;
 }
 
-VirtualServer Client::getServerConfigs(void) const
+ServerLog		CgiHandler::getLog() const
 {
-	return this->_serverConfigs;
+	return this->_log;
 }
 
-RequestParser Client::getRequest(void) const
-{
-	return this->_requestParser;
-}
-
-bool		Client::getRequestRead(void) const
-{
-	return this->_requestRead;
-}
-
-bool		Client::getFirstTimeRequest(void) const
-{
-	return this->_firstTimeRequest;
-}
-
-ssize_t Client::getTotalBodyBytes(void) const
-{
-	return this->_totalBodyBytes;
-}
-
-struct sockaddr_in *Client::getClientAddrPointer(void)
-{
-    return &_client_addr;
-}
-
-socklen_t *Client::getClientAddrLenPointer(void)
-{
-    return &_client_addr_len;
-}
-
-void Client::setRequestBuffer(const std::string& requestBuffer)
-{
-    this->_requestBuffer = requestBuffer;
-}
-
-void Client::setClientSocket(const int& clientSocket)
-{
-   this->_clientSocket = clientSocket;
-}
-
-void Client::setClientAddrLen(const socklen_t& client_addr_len)
-{
-    this->_client_addr_len = client_addr_len;
-}
-
-void Client::setBytesRead(const ssize_t& bytesRead)
-{
-    this->_bytesRead = bytesRead;
-}
-
-void Client::setClientAddr(const struct sockaddr_in& client_addr)
-{
-	this->_client_addr = client_addr;
-}
-
-void Client::setServerConfigs(const VirtualServer& serverConfigs)
-{
-	this->_serverConfigs = serverConfigs;
-}
-
-void Client::setRequest(const RequestParser& request)
-{
-	this->_requestParser = request;
-}
-
-void Client::setRequestRead(const bool& requestRead)
-{
-	this->_requestRead = requestRead;
-}
-
-void Client::setFirstTimeRequest(const bool& firstTimeRequest)
-{
-	this->_firstTimeRequest = firstTimeRequest;
-}
-
-std::string Client::u_int8_to_string(const u_int8_t* data, size_t size)
-{
-    return std::string(reinterpret_cast<const char*>(data), size);
-}
-
-int Client::countBytesUntilCRLF(const u_int8_t* data, int dataSize) const
-{
-    int count = 0;
-    for (int i = 0; i < dataSize - 3; ++i)
+CgiHandler::~CgiHandler() {
+    for (std::vector<char*>::iterator it = _env.begin(); it != _env.end(); ++it)
 	{
-        if (data[i] == '\r' && data[i + 1] == '\n' && data[i + 2] == '\r' && data[i + 3] == '\n')
-		{
-            count += i + 4; // Incluir os bytes da sequência "\r\n\r\n"
-            break;
-        }
+        delete[] *it;
     }
-    return count;
+    _env.clear();
 }
 
-short int	Client::receiveRequest(int client)
+std::string CgiHandler::getScriptFilename(const std::string& requestURI) {
+	size_t lastSlashPos = requestURI.find_last_of("/");
+	if (lastSlashPos == std::string::npos)
+		return (requestURI);
+	return (requestURI.substr(lastSlashPos + 1));
+}
+
+std::vector<char*> CgiHandler::createEnv(std::map<std::string, std::string> requestHeaders, Client client) {
+	std::string clientIp;
+	std::string clientPort;
+	
+	clientIp = inet_ntoa(client.getClientAddrPointer()->sin_addr);
+	clientPort = ntohs(client.getClientAddrPointer()->sin_port);
+	_env.push_back(strdup(("CONTENT_TYPE=" + requestHeaders["Content-Type"]).c_str()));
+	_env.push_back(strdup(("CONTENT_LENGTH=" + requestHeaders["Content-Length"]).c_str()));
+	_env.push_back(strdup(("REQUEST_URI=" + _request.getUri()).c_str()));
+	_env.push_back(strdup(("SCRIPT_NAME=" + _request.getUri().substr(1)).c_str()));
+	_env.push_back(strdup(("SCRIPT_FILENAME=" + getScriptFilename(_request.getUri())).c_str()));
+
+	_env.push_back(strdup(("REMOTE_ADDR=" + clientIp + ":" + clientPort).c_str()));
+	_env.push_back(strdup(("SERVER_NAME=" + requestHeaders["Host"]).c_str()));
+	_env.push_back(strdup(("SERVER_PORT=" + _request.getPortNumber()).c_str()));
+
+	_env.push_back(strdup("AUTH_TYPE=Basic"));
+	_env.push_back(strdup("REQUEST_METHOD=POST"));
+	_env.push_back(strdup("REDIRECT_STATUS=200"));
+	_env.push_back(strdup("DOCUMENT_ROOT=./"));
+	_env.push_back(strdup("GATEWAY_INTERFACE=CGI/1.1"));
+	_env.push_back(strdup("PATH_INFO="));
+	_env.push_back(strdup("PATH_TRANSLATED=.//"));
+	_env.push_back(strdup("QUERY_STRING="));
+	_env.push_back(strdup("SERVER_PROTOCOL=HTTP/1.1"));
+	_env.push_back(strdup("SERVER_SOFTWARE=AMANIX"));
+	_env.push_back(NULL);
+
+	return (_env);
+}
+
+std::string CgiHandler::extractQueryString(const std::string& uri)
 {
-	u_int8_t	buffer[4096];
-	int		bytes;
-	ssize_t	headerBytes;
+    std::string queryString;
 
-	memset(buffer, 0, sizeof(u_int8_t) * 4096);
+    size_t pos = uri.find('?');
 
-	bytes = recv(client, buffer, 4095, 0);
-	if (bytes == -1)
-		return false;
-	else if (bytes == 0)
-	{
-		std::cout << "Client disconnected" << std::endl;
-		return false;
-	}
+    if (pos != std::string::npos) {
+        queryString = uri.substr(pos + 1);
+    }
+
+    return (queryString);
+}
+
+int CgiHandler::getCgi()
+{
+	//decidir o que fazer no get
+	return (0);
+}
+
+int CgiHandler::postCgi(Client client)
+{
+	int response_pipe[2];
+	std::vector<char*> headerEnv = createEnv(_request.getHeaders(), client);
+
+	if (pipe(response_pipe) == -1)
+    {
+        std::cerr << "Error creating pipe: " << strerror(errno) << std::endl;
+    	exit(1);
+    }
+	if (pipe(_request_pipe) == -1)
+    {
+        std::cerr << "Error creating pipe: " << strerror(errno) << std::endl;
+    	exit(1);
+    }
+
+	antiBlock(_request_pipe, response_pipe);
 	
-//	std::cout << buffer << "\nFim do buffer\n" << std::endl;
-	
-	if (this->_firstTimeRequest == true)
-	{
-		this->_requestParser.parse(u_int8_to_string(buffer, bytes));
+	if (!writePipes(_request.getNewRequestBody(), _request.getContentLength())) {
+        return (1);
 	}
 
-	if (this->_requestParser.getMethod() == "GET" \
-		|| this->_requestParser.getMethod() == "DELETE")
+	_pid = fork();
+
+	if (_pid == -1)
 	{
-		this->setRequestRead(true);
-		return true;
+		std::cerr << "Error on fork: " << strerror(errno) << std::endl;
+		exit (1);
 	}
-
-	if (this->_requestParser.getMethod() == "POST" && this->_firstTimeRequest == true)
+	else if (_pid == 0)
 	{
-		headerBytes = countBytesUntilCRLF(buffer, bytes);
-		_totalBodyBytes += bytes - headerBytes;
-
-		// fazer função nova para fazer append da posição do fim dos headers(inicio do body) na string newRequestBody
-		this->_requestParser.startBody(bytes, headerBytes, buffer); //feito
-
-		if (this->_totalBodyBytes == this->_requestParser.getContentLenght())
-		{
-//			std::cout << "NewBody:\n" << this->_requestParser.getNewRequestBody() << std::endl;
-			this->setRequestRead(true);
-			return true;
+		std::vector<char*> argv;
+		std::string path;
+		path = _request.getUri().substr(1);
+		argv.push_back(strdup(path.c_str()));
+		argv.push_back(NULL);
+		if (access(path.c_str(), X_OK) == -1) {
+			std::cerr << "Error on access: " << strerror(errno) << std::endl;
+			exit(1);
 		}
+		if (close(_request_pipe[1]) == -1) {
+  			std::cerr << "Error on close: " << strerror(errno) << std::endl;
+  			exit(1);
+		}
+		if (close(response_pipe[0]) == -1) {
+  			std::cerr << "Error on close: " << strerror(errno) << std::endl;
+  			exit(1);
+		}
+		if (dup2(_request_pipe[0], STDIN_FILENO) == -1) {
+  			std::cerr << "Error on dup2: " << strerror(errno) << std::endl;
+  			exit(1);
+		}
+		if (close(_request_pipe[0]) == -1) {
+  			std::cerr << "Error on close: " << strerror(errno) << std::endl;
+  			exit(1);
+		}
+		if (dup2(response_pipe[1], STDOUT_FILENO) == -1) {
+  			std::cerr << "Error on dup2: " << strerror(errno) << std::endl;
+  			exit(1);
+		}
+		if (close(response_pipe[1]) == -1) {
+  			std::cerr << "Error on close: " << strerror(errno) << std::endl;
+  			exit(1);
+		}
+		_log.createLog();
+		if (execve(path.c_str(), argv.data(), headerEnv.data()) == -1) {
+			std::cerr << "Error on execve: " << strerror(errno) << std::endl;
+			exit(1);
+		}
+		return (0);
 	}
-
-	if (this->_requestParser.getMethod() == "POST" && \
-		this->_firstTimeRequest == false)
+	else
 	{
-		this->_requestParser.appendBody(buffer, bytes);
-		this->_totalBodyBytes += bytes;
-//		std::cout << "Contagem total dos bytes: " << this->_totalBodyBytes << std::endl;
-//		std::cout << "Content Lenght requerido: " << this->_requestParser.getContentLenght() << std::endl;
+		close(_request_pipe[0]);
+		close(response_pipe[1]);
+		return (1);
+	}
+	return (0);
+}
+
+void CgiHandler::antiBlock(int *pipe1, int *pipe2)
+{
+    if (fcntl(pipe1[0], F_SETFL, O_NONBLOCK, FD_CLOEXEC) == -1) {
+		std::cerr << "Error on fcntl: " << strerror(errno) << std::endl;
+	}
+	if (fcntl(pipe1[1], F_SETFL, O_NONBLOCK, FD_CLOEXEC) == -1) {
+		std::cerr << "Error on fcntl: " << strerror(errno) << std::endl;
+	}
+	if (fcntl(pipe2[0], F_SETFL, O_NONBLOCK, FD_CLOEXEC) == -1) {
+		std::cerr << "Error on fcntl: " << strerror(errno) << std::endl;
+	}
+	if (fcntl(pipe2[1], F_SETFL, O_NONBLOCK, FD_CLOEXEC) == -1) {
+		std::cerr << "Error on fcntl: " << strerror(errno) << std::endl;
+	}
+}
+
+bool        CgiHandler::writePipes(std::string message, int contentLength)
+{
+    ssize_t	bytesWritten = 0;
+	ssize_t bytesLeftToWrite = static_cast<ssize_t>(contentLength);
+	ssize_t totalBytesWritten = 0;
+	ssize_t	sizeToWrite = 4096;
+
+	if (static_cast<ssize_t>(contentLength) < sizeToWrite)
+	{
+		sizeToWrite = contentLength;
+	}
+	std::cout << "CONTENT LENGTH NO WRITE PIPES: " << contentLength << std::endl;
+
+	while (bytesLeftToWrite != 0)
+	{
+  		bytesWritten = write(_request_pipe[1], message.c_str() + totalBytesWritten, sizeToWrite);
 		
-		if (this->_totalBodyBytes == this->_requestParser.getContentLenght())
+		if (bytesWritten == -1)
+			break;
+		
+		bytesLeftToWrite -= bytesWritten;
+		totalBytesWritten += bytesWritten;
+		if (bytesLeftToWrite < 4096)
 		{
-//			std::cout << this->_requestParser.getNewRequestBody() << std::endl;
-			this->setRequestRead(true);
-			return true;
+			sizeToWrite = 	bytesLeftToWrite;
 		}
+		std::cout << "BYTESWRITTEN NO WRITE PIPES: " << bytesWritten << std::endl;
 	}
-	
-	if (this->_firstTimeRequest == true)
-		this->setFirstTimeRequest(false);
-	return true;
+	std::cout << "TOTAL BYTESWRITTEN NO WRITE PIPES: " << totalBytesWritten << std::endl;
+    if (bytesWritten == -1)
+	{
+		std::cout << "Bytes Writen == -1" << std::endl;
+        return (false);
+    }
+
+    if (static_cast<size_t>(totalBytesWritten) != message.size())
+	{
+		std::cout << "Bytes Writen != message.size()" << std::endl;
+        return (false);
+    }
+
+    return (true);
 }
